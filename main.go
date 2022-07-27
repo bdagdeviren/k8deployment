ddb603d0c1fe4dc7e78ade3b942b53b3a6275fbb
Your Name <you@example.com>
2022-07-27 21:25:55 +0300

    CheckPodStatus added

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
+import "C"
 import (
 	"bufio"
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"github.com/pkg/errors"
 	"io"
 	"io/ioutil"
@@ -17,17 +19,16 @@ import (
 	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/discovery/cached/memory"
 	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/restmapper"
-	"k8s.io/client-go/tools/clientcmd"
-	"log"
 	"os"
-	"path/filepath"
 	"sigs.k8s.io/yaml"
+	"strings"
 )
 
 type Deployments struct {
-	Wait        int64        `yaml:"wait"`
+	WaitTime    int64        `yaml:"waitTime"`
 	Deployments []Deployment `yaml:"deployments"`
 }
 
@@ -36,11 +37,17 @@ type Deployment struct {
 	Enable     bool         `yaml:"enable"`
 	Path       string       `yaml:"path"`
 	Monitoring []Monitoring `yaml:"monitoring"`
+	Wait       Wait         `yaml:"wait"`
 }
 
 type Monitoring struct {
 	Type string `yaml:"type"`
 	Url  string `yaml:"url"`
+}
+
+type Wait struct {
+	Name      string
+	Namespace string
 }
 
 func DecodeYAML(data []byte) (<-chan *unstructured.Unstructured, <-chan error) {
@@ -160,15 +167,64 @@ func ApplyYaml(config *rest.Config, yamlFile string) error {
 
 }
 
+func Client() (*rest.Config, *kubernetes.Clientset, error) {
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return nil, nil, err
+	}
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return config, clientset, nil
+}
+
+func CheckPodStatus(namespace string, podName string) error {
+	_, clientset, err := Client()
+	if err != nil {
+		return err
+	}
+
+	isDeployed := false
+
+	pods, err := clientset.CoreV1().Pods(C.GoString(namespace)).List(context.Background(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	for _, element := range pods.Items {
+		if strings.Contains(element.Name, C.GoString(podName)) {
+			if element.Status.Phase == "Running" {
+				isDeployed = true
+			} else {
+				isDeployed = false
+			}
+		}
+	}
+
+	resultGo := ""
+	if isDeployed != false {
+		resultGo = "Deployment Successfully - Name: " + podName + ", Namespace: " + namespace
+	} else {
+		resultGo = "Waiting Pod Deployment - Name: " + podName + ", Namespace: " + namespace
+	}
+
+	fmt.Println(resultGo)
+
+	return nil
+}
+
 func main() {
 
-	kubeconfig := filepath.Join(
-		os.Getenv("HOME"), ".kube", "config",
-	)
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		log.Fatal(err)
-	}
+	//kubeconfig := filepath.Join(
+	//	os.Getenv("HOME"), ".kube", "config",
+	//)
+	//config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
 	//
 
 	data, err := ioutil.ReadFile("deployment.yaml")
@@ -186,11 +242,13 @@ func main() {
 	//fmt.Printf("%+v", deployments)
 
 	for _, element := range deployments.Deployments {
+		fmt.Printf("%+v \n", element)
 		if element.Name != "" && element.Path != "" && element.Enable != false {
-			err = ApplyYaml(config, element.Path)
-			if err != nil {
-				println(err.Error())
-			}
+
+			//err = ApplyYaml(config, element.Path)
+			//if err != nil {
+			//	println(err.Error())
+			//}
 		}
 	}
 
